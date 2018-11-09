46302d6a5dd893b1e504aa2c64292dcac374bb2a
Gareth Cheeseman <Gareth.Cheeseman@stormid.com>
2018-11-09 16:13:27 +0000

    Adds Selenium, ChromeDriverFixture, Xunit, JsonDataAttribute, and ElementCheck

---
+++ WarmUpTests/ChromeDriverFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WarmUpTests
+{
+    public class ChromeDriverFixture : IDisposable
+    {
+        public IWebDriver Driver { get; }
+
+        public ChromeDriverFixture()
+        {
+            var options = new ChromeOptions();
+                options.AddArgument("headless");
+            
+            Driver = new ChromeDriver(options);
+        }
+
+        public void Dispose()
+        {
+            Driver?.Dispose();
+        }
+    }
+}
---
+++ WarmUpTests/ElementCheck.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace WarmUpTests
+{
+    public class ElementCheck : IClassFixture<ChromeDriverFixture>
+    {
+        private readonly ChromeDriverFixture _fixture;
+
+        public ElementCheck(ChromeDriverFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Theory]
+        [JsonFileData("data.json")]
+        public void CheckElementExists(string url, string xpath, bool expectedResult)
+        {
+            _fixture.Driver.Navigate().GoToUrl(url);
+
+            bool actualResult;
+
+            try
+            {
+                var element = _fixture.Driver.FindElement(By.XPath(xpath));
+                actualResult = element.Displayed;
+
+            }
+            catch (Exception)
+            {
+                actualResult = false;
+            }
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
