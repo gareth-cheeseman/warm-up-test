a14b90c5b211f5667aa2966fc974ff3bdb5d04e2
Gareth Cheeseman <Gareth.Cheeseman@stormid.com>
2019-02-07 14:20:03 +0000

    Comments out reading from appsettings

--- WarmUpTests/Fixture.cs
+++ WarmUpTests/Fixture.cs
@@ -19,25 +19,25 @@ namespace WarmUpTests
 
         public Fixture()
         {
-            using (var file = File.OpenText("Properties\\launchSettings.json"))
-            {
-                var reader = new JsonTextReader(file);
-                var jObject = JObject.Load(reader);
-
-                var variables = jObject
-                    .GetValue("profiles")
-                    //select a proper profile here
-                    .SelectMany(profiles => profiles.Children())
-                    .SelectMany(profile => profile.Children<JProperty>())
-                    .Where(prop => prop.Name == "environmentVariables")
-                    .SelectMany(prop => prop.Value.Children<JProperty>())
-                    .ToList();
-
-                foreach (var variable in variables)
-                {
-                    Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
-                }
-            }
+//            using (var file = File.OpenText("Properties\\launchSettings.json"))
+//            {
+//                var reader = new JsonTextReader(file);
+//                var jObject = JObject.Load(reader);
+//
+//                var variables = jObject
+//                    .GetValue("profiles")
+//                    //select a proper profile here
+//                    .SelectMany(profiles => profiles.Children())
+//                    .SelectMany(profile => profile.Children<JProperty>())
+//                    .Where(prop => prop.Name == "environmentVariables")
+//                    .SelectMany(prop => prop.Value.Children<JProperty>())
+//                    .ToList();
+//
+//                foreach (var variable in variables)
+//                {
+//                    Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+//                }
+//            }
 
             BaseUrl = Environment.GetEnvironmentVariable("BaseUrl");
 
